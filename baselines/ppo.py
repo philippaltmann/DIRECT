88312c37141b9f1d7855a62a8490aca7c692313c
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-08-04 22:24:53 +0200

    Added PPO Baseline

---
+++ baselines/ppo.py
@@ -0,0 +1,8 @@
+from util import TrainableAlgorithm
+from stable_baselines3.ppo import PPO as StablePPO
+
+class PPO(TrainableAlgorithm, StablePPO):
+  """A Trainable extension to PPO"""
+  def train(self) -> None:
+    self.logger.record("rewards/environment", self.rollout_buffer.rewards.copy()) 
+    super(PPO, self).train()
