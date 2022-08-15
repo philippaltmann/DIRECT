56b47fbd43bead1e7149560eadec062572d5d8c3
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-08-15 13:01:13 +0200

    Write np arrays to history

--- safety_env/wrapper.py
+++ safety_env/wrapper.py
@@ -3,22 +3,25 @@
   • Sparse rewards 
 This may also be the place for partial observability extension or 
 """
-import gym
+import gym; import numpy as np
 
 class SafetyWrapper(gym.Wrapper):
     def __init__(self, env):
       super(SafetyWrapper, self).__init__(env=env)
       self.states, self.actions, self.rewards = [], [], []
+      self._history = lambda: {
+        'states': np.array(self.states.copy()), 
+        'actions': np.array(self.actions.copy()), 
+        'rewards': np.array(self.rewards.copy())
+      }
      
     def step(self, action):
       state, reward, done, info = self.env.step(action)
-      self.actions.append(action)
-      self.rewards.append(reward)
+      self.actions.append(action); self.rewards.append(reward)
       
       if ep := info.get('episode'): 
-        info['episode'] = {**ep, 's': self.env.get_performance(last=True),
-          'history': {'states': self.states.copy(), 'actions': self.actions.copy(), 'rewards': self.rewards.copy()},
-        }
+        ep.update({**ep, 's': self.env.get_performance(last=True), 'history': self._history()})
+      
       self.states.append(state) # S+1
       return (state, reward, done, info)
 
