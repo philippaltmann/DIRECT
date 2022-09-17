9df289cc2869894be4ec965726fd81683f51d125
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-09-17 14:30:09 +0200

    Added defaults, fixed running baselines

--- run/__main__.py
+++ run/__main__.py
@@ -1,35 +1,37 @@
 import argparse; import setuptools; import os; import random
 from safety_env import factory; from util import TrainableAlgorithm
-from direct import DIRECT; from baselines import PPO; 
+from direct import DIRECT; from baselines import DQN, PPO
 parser = argparse.ArgumentParser()
 
 # General Arguments
-parser.add_argument('algorithm', type=str, help='The algorithm to use', choices=['DIRECT', 'PPO'])
+parser.add_argument('algorithm', type=str, help='The algorithm to use', choices=['DIRECT', 'A2C', 'DQN', 'HER', 'PPO'])
 parser.add_argument( '--env', nargs='+', default=['DistributionalShift', 0, 4, 1], metavar="Environment",
   help='The name and spec and of the safety environments to train and test the agent. Usage: --env NAME, CONFIG, N_TRAIN, N_TEST')
 parser.add_argument('-s', dest='seed', type=int, help='The random seed. If not specified a free seed [0;999] is randomly chosen')
 parser.add_argument('--load', help='Whether to load a model or train a new one.', action='store_true')
 
 # Training Arguments
-parser.add_argument('-t', dest='timesteps', type=float, default=8192, help='Number of timesteps to learn the model (eg 10e4)')
+parser.add_argument('-t', dest='timesteps', type=float, default=10e4, help='Number of timesteps to learn the model (eg 10e4)')
 parser.add_argument('-ts', dest='maxsteps', type=float, help='Maximum timesteps to learn the model (eg 10e4), using early stopping')
 parser.add_argument('--reward-threshold', type=float, help='Threshold for 100 episode mean return to stop training.')
 
 # DIRECT Specific Arguments  
-parser.add_argument('--chi', type=float, help='The mixture parameter determining the mixture of real and discriminative reward. (default: 1.0)')
-parser.add_argument('--kappa', type=int, help='Number of trajectories to be stored in the direct buffer. (default: 512)')
-parser.add_argument('--omega', type=float, help='The frequency to perform discriminator updates in relation to policy updates. (default: 1/1)')
+parser.add_argument('--chi', type=float, default=1.0, help='The mixture parameter determining the mixture of real and discriminative reward. (default: 1.0)')
+parser.add_argument('--kappa', type=int, default=512, help='Number of trajectories to be stored in the direct buffer. (default: 512)')
+parser.add_argument('--omega', type=float, default=1.0, help='The frequency to perform discriminator updates in relation to policy updates. (default: 1/1)')
 
 # Policy Optimization Arguments
 parser.add_argument('--n-steps', type=int, help='The length of rollouts to perform policy updates on')
 
 # Get arguments 
 args = {key: value for key, value in vars(parser.parse_args()).items() if value is not None}
+if args['algorithm'] != "DIRECT": [args.pop(key) for key in ['chi', 'kappa', 'omega']]
+hp_suffix = f"/{args['chi']}_{args['omega']}_{args['kappa']}" if args['algorithm'] == "DIRECT" else ''
 
 # Get path & seed, create model & envs
 algorithm = eval(args.pop('algorithm')); model = None
 env = dict(zip(['name', 'spec', 'n_train', 'n_test'], args.pop('env')))
-base_path = lambda seed: f"results/{algorithm.__name__}/{env['name']}/{seed}/"
+base_path = lambda seed: f"results/{algorithm.__name__}/{env['name']}{hp_suffix}/{seed}/"
 gen_seed = lambda s=random.randint(0, 999): s if not os.path.isdir(base_path(s)) else gen_seed()
 seed = args.pop('seed', gen_seed()); path = base_path(seed); envs = factory(seed, **env)
 
@@ -44,4 +46,4 @@ args = {'envs': envs, 'path': path, 'seed': seed, **args}
 load = args.pop('load', False) #; print(f"{'load' if load else 'creat'}ing model  with args {args}")
 model:TrainableAlgorithm = algorithm.load(**args) if load else algorithm(**args)
 model.learn(total_timesteps=timesteps, stop_on_reward=stop_on_reward, reset_num_timesteps = not load)
-model.save()
+if path: model.save()
