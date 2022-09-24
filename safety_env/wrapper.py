8dd3b851264e987b928bd2366110fcc506d70e6b
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-09-24 15:20:26 +0200

    Added state action iteration to safety env wrapper

--- safety_env/wrapper.py
+++ safety_env/wrapper.py
@@ -4,6 +4,7 @@
 This may also be the place for partial observability extension or 
 """
 import gym; import numpy as np
+from ai_safety_gridworlds.environments.shared import safety_game
 
 class SafetyWrapper(gym.Wrapper):
     def __init__(self, env, sparse=True):
@@ -30,3 +31,31 @@ class SafetyWrapper(gym.Wrapper):
       state = self.env.reset(game_art=game_art)
       self.states, self.actions, self.rewards = [state], [], []
       return state
+
+    def iterate(self, function = lambda e,s,a,r: r(), agent='A', field=' ', fallback=None):
+      """Iterate all possible actions in all env states, apply `funcion(env, state, action)`
+      function: `f(env, state, action, reward()) => value` to be applied to all actions in all states 
+        default: return envreward
+      fields: Mapping for fields to replace, agent: Mapping of agent field to move in env
+      fallback: result to put in fields where agent can't be moved (eg. walls)
+      :returns: ENVxACTIONS shaped function results"""
+
+      # Get env object, prepare mappings for env generation 
+      safety_env = self.env.env.env; empty_board = self.reset()[0]
+      value_mapping = safety_env._env._value_mapping; act = safety_game.Actions
+      agent_value = value_mapping[agent]; field_value = value_mapping[field]
+      actions = [act.UP, act.RIGHT, act.DOWN, act.LEFT]; fallback = [fallback] * len(actions)
+      lookup = {v: k for k, v in value_mapping.items()}
+      
+      # Create empty board for iteration & function for reverting Observation to board 
+      empty_board[empty_board == agent_value] = field_value
+      board = lambda state: ["".join([lookup[cell] for cell in row]) for row in state]
+      reward = lambda action: lambda: self.step(action)[1]
+      def prepare(x,y): state = empty_board.copy(); state[y][x] = agent_value; return state
+      process = lambda state: [function(self, self.reset(board(state)), action, reward(action)) for action in actions]
+      
+      return [ 
+        [ process(prepare(x,y)) if cell == field_value else fallback for x, cell in enumerate(row) ] 
+          for y, row in enumerate(empty_board)
+      ]
+      
