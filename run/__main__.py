91522a7484d1b67c06636f4ddf6d0dd792e45916
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-09-25 17:20:57 +0200

    Updated runner

--- run/__main__.py
+++ run/__main__.py
@@ -1,15 +1,16 @@
 import argparse; import setuptools; import os; import random
-from safety_env import factory; from util import TrainableAlgorithm
-from direct import DIRECT; from baselines import DQN, PPO
+from safety_env import factory; from common import TrainableAlgorithm
+from direct import DIRECT; from baselines import A2C, DQN, PPO, VPG
 parser = argparse.ArgumentParser()
 
 # General Arguments
-parser.add_argument('algorithm', type=str, help='The algorithm to use', choices=['DIRECT', 'A2C', 'DQN', 'HER', 'PPO'])
+parser.add_argument('algorithm', type=str, help='The algorithm to use', choices=['DIRECT', 'A2C', 'DQN', 'PPO', 'VPG'])
 parser.add_argument( '--env', nargs='+', default=['DistributionalShift', 0, 4, 1], metavar="Environment",
   help='The name and spec and of the safety environments to train and test the agent. Usage: --env NAME, CONFIG, N_TRAIN, N_TEST')
 parser.add_argument('-s', dest='seed', type=int, help='The random seed. If not specified a free seed [0;999] is randomly chosen')
 parser.add_argument('--load', help='Whether to load a model or train a new one.', action='store_true')
 parser.add_argument('--test', help='Run in test mode (dont write log files).', action='store_true')
+parser.add_argument('--path', default='results', help='The base path, defaults to `results`')
 
 # Training Arguments
 parser.add_argument('-t', dest='timesteps', type=float, default=10e4, help='Number of timesteps to learn the model (eg 10e4)')
@@ -27,12 +28,12 @@ parser.add_argument('--n-steps', type=int, help='The length of rollouts to perfo
 # Get arguments 
 args = {key: value for key, value in vars(parser.parse_args()).items() if value is not None}
 if args['algorithm'] != "DIRECT": [args.pop(key) for key in ['chi', 'kappa', 'omega']]
-hp_suffix = f"/{args['chi']}_{args['omega']}_{args['kappa']}" if args['algorithm'] == "DIRECT" else ''
+hp_suffix = f"{args['chi']}_{args['omega']}_{args['kappa']}" if args['algorithm'] == "DIRECT" else 'baseline'
 
 # Get path & seed, create model & envs
 algorithm = eval(args.pop('algorithm')); model = None
 env = dict(zip(['name', 'spec', 'n_train', 'n_test'], args.pop('env')))
-base_path = lambda seed: f"results/{algorithm.__name__}/{env['name']}{hp_suffix}/{seed}/"
+base_path = lambda seed: f"{args.path}/{algorithm.__name__}/{env['name']}/{hp_suffix}/{seed}/"
 gen_seed = lambda s=random.randint(0, 999): s if not os.path.isdir(base_path(s)) else gen_seed()
 seed = args.pop('seed', gen_seed()); path = base_path(seed); envs = factory(seed, **env)
 if args.pop('test'): path = None
