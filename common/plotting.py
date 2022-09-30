Fixed heatmap scaling, added plotly helpers

common/plotting.py
@@ -1,13 +1,10 @@
 import numpy as np; import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.tri import Triangulation
+import plotly.graph_objects as go
 
-import os; import itertools; from parse import parse; from tqdm import tqdm
-from tensorboard.backend.event_processing.event_accumulator import EventAccumulator as EA
-from os import path
 
-def triangle_heatmap(data, min=0, max=1):
-  # TODO: automatic min max scaling 
+def triangle_heatmap(data, vmin=0, vmax=1):
   figure, ax = plt.subplots(figsize=(8,6))
   # f = plt.figure(figsize=fig_size)
   ct = lambda c,r: (c,r) # Helper points at c(ollumn), r(ow) for w(idth)
@@ -23,11 +20,11 @@ def triangle_heatmap(data, min=0, max=1):
   labels = lambda c,r: np.array([uc(c,r),rc(c,r),dc(c,r),lc(c,r)])
   points = lambda c,r: np.array([ct(c,r),ul(c,r),ur(c,r),dr(c,r),dl(c,r)])
   square = lambda c,r: Triangulation(*points(c,r).T, triangles)
-  # plot = lambda v,c,r: ax.tripcolor(square(c,r), v, cmap='Spectral',vmin=-51, vmax=49, ec='w') #vmin=0, vmax=1, RdYlGn
-  # text = lambda v,c,r: [ax.text(x,y, f'{v:.0f}', color='k' if -10 < v < 10 else 'w', ha='center', va='center') for x,y,v in zip(*labels(c,r).T, v)]
   
-  plot = lambda v,c,r: ax.tripcolor(square(c,r), v, cmap='Spectral', vmin=min, vmax=max, ec='w') #, RdYlGn
-  center = min + (max - min) / 2; width = abs(min) + abs(max)
+  flat = [v for row in data for col in row for v in col if v is not None]
+  vmin = vmin or min(flat); vmax = vmax or max(flat)
+  plot = lambda v,c,r: ax.tripcolor(square(c,r), v, cmap='Spectral', vmin=vmin, vmax=vmax, ec='w') #, RdYlGn
+  center = vmin + (vmax - vmin) / 2; width = abs(vmin) + abs(vmax)
   lcolor = lambda v: 'k' if center - 0.3 * width < v < center + 0.3 * width else 'w'
   _txt = lambda v,c,r: ax.text(c,r, f'{v:.2f}', color=lcolor(v), ha='center', va='center')
   text = lambda v,c,r: [_txt(v,x,y) for x,y,v in zip(*labels(c,r).T, v)]
@@ -43,44 +40,32 @@ def triangle_heatmap(data, min=0, max=1):
   return figure
 
 
-def fetch_experiments(base='./results', alg=None, env=None, metrics=[], load_csv=False, dump_csv=False):
-  """Loads and structures all tb log files Given:
-  :param base_path (str):
-  :param env (optional): the environment to load 
-  :param alg (optional): the algorithm to load 
-  :param metrics: list of (Name, Tag) tuples of metrics to load
-  :param save_csv: save loaded experiments to csv
-  Returns: list with dicts of experiments 
-  """
-  # Helper to fetch all relevant folders 
-  subdirs = lambda dir: [d for d in os.scandir(dir) if d.is_dir()]
+# Helper functions to create scatters/graphs from experiment & metric
+def plot_box(title, plot):
+  box = lambda g: go.Box(name=g['label'], y=g['data'], marker_color=color(g['hue']), boxmean=True)  #boxmean='sd'
+  return { 'layout': layout(title, False), 'data': [ box(g) for g in plot['graphs'] ] }
 
-  print(f"Scanning for {alg if alg else 'algorithms'} in {base}")  # First layer: Algorithms
-  if alg: experiments = [{'algorithm': alg, 'path':f'{base}/{alg}'}]
-  else: experiments = [{'algorithm': a.name, 'path': a} for a in subdirs(base)]
+# reward_threshold = {'type':'line', 'x0':0, 'y0':0.945, 'x1':1000000, 'y1':0.945, 'line':{'color':'#424242', 'width':2, 'dash':'dash'}}
 
-  print(f"Scanning for {env if env else 'environments'} in {base}")  # Second layer: Environments
-  if env: experiments = [{**exp, 'env': env, 'path': f'{exp["path"]}/{env}'} for exp in experiments]
-  else: experiments = [{**exp, 'env': e.name, 'path': e} for exp in tqdm(experiments) for e in subdirs(exp['path'])]
+def plot_ci(title, plot):
+  scatter = lambda data, **kwargs: go.Scatter(x=data.index, y=data, **kwargs)
+  getmean = lambda g: scatter(g['data'][0], name=g['label'], mode='lines', line={'color': color(g['hue']), 'smoothing': 1.0})
+  getconf = lambda g: scatter(g['data'][1], fillcolor=color(g['hue'], True), fill='toself', line={'color': 'rgba(255,255,255,0)'}, showlegend=False)
+  #
 
-  print(f"Scanning for hyperparameters in {base}")  # Third layer: Hyperparameters & number of runs
-  hp = lambda name: dict(zip(['chi','omega','kappa'], parse('{:.1f}_{:.1f}_{:d}', name) or []))
-  experiments = [{ **exp, 'path': e.path, **hp(e.name), 'runs': len(subdirs(e)) }
-    for exp in tqdm(experiments) if os.path.isdir(exp['path']) for e in subdirs(exp['path'])
-  ]
+  return { 'layout': layout(title), 'data': [getconf(g) for g in plot['graphs']] + [getmean(g) for g in plot['graphs']] }
 
-  print(f"Loading experiment logfiles from {metrics}")  # Fourth layer: tb files   
-  csv = lambda run, name: path.isfile(f'{run.path}/{name}.csv')
-  extract_args = {'columns': ['Time', 'Step', 'Data'], 'index': 'Step', 'exclude': ['Time']}
-  extract_data = lambda run, key : pd.DataFrame.from_records(EA(run.path).Reload().Scalars(key), **extract_args)
-  extract_csvs = lambda run, name: pd.read_csv(f'{run.path}/{name}.csv').set_index('Step')
-  load_scalars = lambda run, name, key: extract_csvs(run, name) if csv(run, name) else extract_data(run, key)
-  process_data = lambda exp, name, key: [ load_scalars(run, name, key) for run in tqdm(subdirs(exp['path'])) ] 
 
-  # Process given experiments
-  experiments = [{**exp, 'data': { name: process_data(exp, name, key) for name, key in metrics } } for exp in tqdm(experiments)]
+#                      Red         Orange     Green          Blue        Purple 
+# color_lookup = {"PPO": 340, "A2C": 40, "SIL": 130, "DIRECT": 210, "DQN": 290 }
+def color(hue, sec=False): return 'hsva({},{}%,{}%,{})'.format(hue, 90-sec*20, 80+sec*20, 1.0-sec*0.8)
 
-  dump_experiment = lambda data, runs: [ df.to_csv(f'{r.path}/{m}.csv') for m, d in data for r, df in zip(runs, d) ]
-  if dump_csv: [dump_experiment(exp['data'].items(), subdirs(exp['path'])) for exp in experiments]
 
-  return experiments
+def layout(title=None, legend=True): 
+  return go.Layout( title=title, showlegend=legend, font=dict(size=24), 
+    margin=dict(l=8, r=8, t=8+(72 * (title is not None)), b=8), width=1200+300*legend, height=600)
+
+
+def generate_figures(plots, generator):
+  title = lambda plot: f'{plot["metric"]} ({plot["title"]})'
+  return { title(plot): go.Figure(**generator[plot['metric']](title(plot), plot)) for plot in plots }