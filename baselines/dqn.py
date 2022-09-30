efd548b1e71e71322f18dd59d0e6889e9e0cc302
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-09-30 16:11:10 +0200

    Fixed DQN heatmap

--- baselines/dqn.py
+++ baselines/dqn.py
@@ -3,13 +3,12 @@ from stable_baselines3.dqn import DQN as StableDQN
 
 class DQN(TrainableAlgorithm, StableDQN):
   """A Trainable extension to DQN"""
-  def __init__(self, **kwargs): super(DQN, self).__init__(**kwargs); self.n_steps, self.prev_steps = None, 0
+  def __init__(self, **kwargs): super(DQN, self).__init__(**kwargs); self.n_steps = self.train_freq[0]
 
   def _setup_model(self) -> None:
     super(DQN, self)._setup_model()
     self.get_actions = lambda s: self.policy.q_net.forward(s).cpu().detach().numpy()
 
   def train(self, **kwargs) -> None:
-    self.n_steps = (self.num_timesteps - self.prev_steps) / self.env.num_envs
     self.logger.record("rewards/environment", self.replay_buffer.rewards.copy()) 
     super(DQN, self).train(**kwargs); self.prev_steps = self.num_timesteps
