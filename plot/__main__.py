70f02ab43bad6ab409d54fcab34f741551c3a317
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-05 12:06:48 +0200

    Added metrics to plot options

--- plot/__main__.py
+++ plot/__main__.py
@@ -1,6 +1,8 @@
 """
 Example commands:
 python -m plot experiments/1-HP/sar -g algorithm env omega
+python -m plot experiments/2-benchmark -g env
+python -m plot experiments/2-benchmark -g env -a PPO -m Heatmap
 python -m plot experiments/1-HP/sar
 
 TODO: consider smoothing?
@@ -9,29 +11,34 @@ import argparse; import os;
 from plot.metrics import *
 from common.plotting import * 
 
+options = { # Title, Scalar(Name, Tag), process(scalar)->data, display(data)->trace
+  'Reward': (('Reward', 'metrics/validation_reward'), process_ci, plot_ci),
+  'Return': (('Return', 'rewards/return-100-mean'), process_ci, plot_ci),
+  'Buffer': (('Buffer', 'rewards/return-100-mean'), process_ci, plot_ci),
+  'Length':  (('Length', 'rewards/length-100-mean'), process_ci, plot_ci),
+  'Steps': (('Reward', 'metrics/validation_reward'),  process_steps, plot_box),
+  'Heatmap': (('Model', 0), process_heatmap, plot_heatmap)
+}
+
 # Process commandline arguments 
 parser = argparse.ArgumentParser()
 parser.add_argument('base', default='./results', help='The results root')
 parser.add_argument('-a', dest='alg', help='Algorithm to vizualise.')
 parser.add_argument('-e', dest='env', help='Environment to vizualise.')
 parser.add_argument('-g', dest='groupby', nargs='+', default=['algorithm', 'env'], metavar="groupby", help='Experiment keys to group plotted data by.')
-args = parser.parse_args()
+parser.add_argument('-m', dest='metrics', nargs='+', default=['Return', 'Steps'], choices=options.keys(), help='Experiment keys to group plotted data by.')
+parser.add_argument('-nd', dest='dump_csv', action='store_false', help='Skip csv dump')
+args = vars(parser.parse_args()); groupby = args.pop('groupby'); 
 
-metrics = [ # Title, Scalar(Name, Tag), process(scalar)->data, display(data)->trace
-  ('Reward', ('Reward', 'metrics/validation_reward'), process_ci, plot_ci),
-  ('Return', ('Return', 'rewards/return-100-mean'), process_ci, plot_ci),
-  ('Buffer', ('Buffer', 'rewards/return-100-mean'), process_ci, plot_ci),
-  ('Length', ('Length', 'rewards/length-100-mean'), process_ci, plot_ci),
-  ('Steps', ('Reward', 'metrics/validation_reward'), process_steps, plot_box)]
+metrics = [(metric, *options[metric]) for metric in args.pop('metrics')]
 titles, scalars, procs, plotters = zip(*metrics)
 
 # Load, sort and group experiments, calculate metrics and generate figures
-experiments = fetch_experiments(args.base, metrics=list(zip(titles, scalars)), dump_csv=True)
-experiments = sorted(experiments, key=lambda exp: (exp['kappa'],exp['omega']))
-experiments = group_experiments(experiments, args.groupby)
+experiments = fetch_experiments(**args, metrics=list(zip(titles, scalars)))
+experiments = group_experiments(experiments, groupby)
 experiments = calculate_metrics(experiments, list(zip(titles, procs)))
 figures = generate_figures(experiments, dict(zip(titles, plotters)))
 
 # Save figures
-out = f'{args.base}/plots/{"-".join(args.groupby)}'; os.makedirs(out, exist_ok=True)
+out = f'{args["base"]}/plots/{"-".join(groupby)}'; os.makedirs(out, exist_ok=True)
 [figure.write_image(f'{out}/{name}.pdf') for name, figure in figures.items()]
