53d4156305b778f7bdd379b363ef13877b110726
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-14 15:09:46 +0200

    Added dense settings to basline tests

--- test/baselines.sh
+++ test/baselines.sh
@@ -1,13 +1,18 @@
+
 ENV="DistributionalShift-Sparse"
 BASE="experiments/2-benchmark"
 OUT="experiments/logfiles/baselines"
 
-for run in 1 2 3 4 5 6 7 8; do
-  for alg in A2C DQN PPO VPG; do
-    echo "Running $alg $run"
-    nohup python -m run $alg -ts 10e5 --env $ENV --path $BASE &> "$OUT/$alg-$run.out" &
-    sleep 1
-  done
-  sleep 20m
-done
+# ENV="DistributionalShift"
+# BASE="experiments/2-benchmark-dense"
+# OUT="experiments/logfiles/baselines-dense"
+sleep 6h
 
+for RUN in 1 2 3 4 5 6 7 8; do # 1
+  for ALG in A2C DQN ; do #PPO VPG
+    echo "Running $ALG $RUN"
+    nohup python -m run $ALG -ts 10e5 --env $ENV --path $BASE &> "$OUT/$ALG-$RUN.out" &
+    # sleep 30m
+  done
+  sleep 30m
+done
