d92d1165ca7a32c6ecc39aad4e62dfd3d44c9e3a
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-21 18:34:01 +0200

    Added SIL

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -1,7 +1,8 @@
 from .a2c import A2C
 from .dqn import DQN
 from .ppo import PPO
+from .sil import SIL
 from .vpg import VPG
 from .direct import DIRECT
 
-ALGS = ['DIRECT', 'A2C', 'DQN', 'PPO', 'VPG']
+ALGS = ['DIRECT', 'A2C', 'DQN', 'PPO', 'SIL', 'VPG']
