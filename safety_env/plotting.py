6ba40da90e9d240103a868ca0227ec0de3dea9f2
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-22 01:09:09 +0200

    Added agent plotting option

--- safety_env/plotting.py
+++ safety_env/plotting.py
@@ -40,7 +40,7 @@ def heatmap_2D(data, vmin=0, vmax=1):
   return figure
 
 
-def heatmap_3D(data, vmin=0, vmax=1):
+def heatmap_3D(data, vmin=0, vmax=1, show_agent=False):
   rows,cols  = data.shape[0:2] # extract iteration data and arguments
   
   # Value range and coloration helpers
@@ -48,7 +48,7 @@ def heatmap_3D(data, vmin=0, vmax=1):
   vmin = min(fltd) if vmin is None else vmin; vmax = max(fltd) if vmax is None else vmax
   pct = lambda v: (np.mean(v)-vmin)/(vmax-vmin); opc = lambda v: 1-np.var(v)/(vmax-vmin)**2*2 # alt: 1-np.std(test)/(10-0)*2
   lookup = {'f':'hsl({:0.0f},{:0.0f}%,80%)', 'g':'hsl(140,100%,50%)', 'l': 'hsl(0,100%,50%)', 'w': 'rgba(192,192,192,0.5)'}
-  color = lambda v: lookup[v] if type(v) == str and v in ['g','l','w'] else lookup['f'].format(pct(v)*140, opc(v)*100)
+  color = lambda v: lookup[v] if type(v) == str and v in ['a','g','l','w'] else lookup['f'].format(pct(v)*140, opc(v)*100)
   # lookup = {'f':'hsla({:0.0f},80%,40%,{:1.2f})', 'g':'hsl(140,100%,60%)', 'l': 'hsl(0,100%,60%)', 'w': 'rgba(192,192,192,0.5)'}
   # color = lambda v: lookup[v] if type(v) == str and v in ['g','l','w'] else lookup['f'].format(pct(v)*120, opc(v)) #140
   t = lambda c,r: 'w' if r in [0,rows-1] or c in [0,cols-1] else 'g' if r in [1,rows-2] and c in [1,cols-2] else 'l'
@@ -71,10 +71,11 @@ def heatmap_3D(data, vmin=0, vmax=1):
   square = lambda c,r: [_adapt(c,r,v) for v in [(1,2,3), (1,3,4), (6,7,8), (5,6,8), (1,2,6), (2,5,6), (2,3,6), (3,6,7), (3,4,8), (3,7,8), (1,4,8), (1,5,8)]]
   field = lambda c,r,val: [] if None in val.flat else [(*triang(c,r,i), color(v)) for i,v in enumerate(val)] 
   cube = lambda c,r,val: [(*f, color(t(c,r))) for f in square(c,r)] if None in val.flat else []
+  agent = lambda c,r,val: [(*f, 'hsl(210,100%,50%)') for f in square(c,r)] if show_agent and c==cols-2 and r==1 else []
 
   process = lambda *f: np.array([result for fn in f for r, row in enumerate(data) for c,val in enumerate(reversed(row)) for result in fn(c,r,val)]).T.tolist()
 
-  p = dict(zip(['x','y','z'], process(points))); f = dict(zip(['i','j','k','facecolor'], process(field, cube)))
+  p = dict(zip(['x','y','z'], process(points))); f = dict(zip(['i','j','k','facecolor'], process(field, cube, agent)))
   l = dict(zip(['x','y','z','text','textfont'], process(txt))); l['textfont'] = {'color': l['textfont'], 'size':8} 
 
   mesh=go.Mesh3d(**p, **f) 
