5b9c48537b417aabe034a2f6fd218685026f8f6e
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-23 19:02:54 +0200

    Updated example commands and arguments

--- plot/__main__.py
+++ plot/__main__.py
@@ -1,9 +1,21 @@
+"""Example commands:
+Test evaluation
+python -m plot plot/results/train -g env --eval 0 1 3
+python -m plot plot/results/train --mergeon algorithm --eval 0 1 3
+
+Generate Training plots:
+python -m plot experiments/1-Train -m Return Steps -g env 
+python -m plot experiments/1-Train -g env -a DIRECT --heatmap 0 1 3
+python -m plot experiments/1-Train --mergeon algorithm --eval 0 1 3
+
+Generate Adaptation plots:
+python -m plot experiments/2-Adapt -b experiments/1-Train -m Return Steps -g env 
+python -m plot experiments/2-Adapt -g env -a DIRECT --heatmap 0 1 3
+python -m plot experiments/2-Adapt --mergeon algorithm --eval 0 1 3
+python -m plot experiments/2-Adapt -g env --eval 0 1 3
 """
-Example commands:
-python -m plot plot/results/reload -b plot/results/train --eval
-python -m plot plot/results/train 
-"""
-import argparse; import os; 
+
+import argparse; import time; import os; 
 from plot.metrics import *
 from plot.plotting import * 
 
@@ -17,7 +29,7 @@ options = { # Title, Scalar(Name, Tag), process(scalar)->data, display(data)->tr
 
 # Process commandline arguments 
 parser = argparse.ArgumentParser()
-parser.add_argument('base', default='./results', help='The results root')
+parser.add_argument('base', help='The results root')
 parser.add_argument('-a', dest='alg', help='Algorithm to vizualise.')
 parser.add_argument('-b', dest='baseline', help='Base path of reloaded model.')
 parser.add_argument('-e', dest='env', help='Environment to vizualise.')
@@ -28,21 +40,18 @@ parser.add_argument('--mergeon', help='Key to merge experiments e.g. algorithm.'
 parser.add_argument('--no-dump', dest='dump_csv', action='store_false', help='Skip csv dump')
 parser.add_argument('--eval', nargs='+', default=[], help='Run Evaluations')
 
-tryint = lambda s: int(s) if s.isdigit() else s
-args = vars(parser.parse_args()) 
+args = vars(parser.parse_args()); tryint = lambda s: int(s) if s.isdigit() else s
 hm = [tryint(s) for s in args.pop('heatmap')]
 groupby = args.pop('groupby'); mergeon = args.pop('mergeon');
 if len(hm): options['Heatmap'] = (('Model', hm), process_heatmap, plot_heatmap); args['metrics'].append('Heatmap') 
 
-enames = ['Evaluation Training', 'Evaluation Shifted Obs', 'Evaluation Shifted Obs 2', 'Evaluation Shifted Goal']
+enames = ['Training', 'Shifted Obs', 'Shifted Obs 2', 'Shifted Goal']
 def add_eval(e): options[enames[e]] = (('Model', e), process_eval, plot_eval); args['metrics'].append(enames[e])
-[add_eval(tryint(e)) for e in args.pop('eval')]
-
+ev = [add_eval(tryint(e)) for e in args.pop('eval')]
 metrics = [(metric, *options[metric]) for metric in args.pop('metrics')]
 titles, scalars, procs, plotters = zip(*metrics)
 label_exclude =  [] if args['alg'] and args['alg'] == 'DIRECT' else ['chi', 'omega', 'kappa']
 
-# Load, sort and group experiments, calculate metrics and generate figures
 experiments = fetch_experiments(**args, metrics=list(zip(titles, scalars)))
 experiments = group_experiments(experiments, groupby, label_exclude, mergeon)
 metrics = calculate_metrics(experiments, list(zip(titles, procs)))
@@ -51,4 +60,6 @@ figures = generate_figures(metrics, dict(zip(titles, plotters)))
 # Save figures
 out = f'{args["base"]}/plots/{"-".join(groupby)}'; os.makedirs(out, exist_ok=True)
 if len(hm): os.makedirs(out+'/Heatmaps', exist_ok=True)
-[figure.write_image(f'{out}/{name}.pdf') for name, figure in figures.items()]
+if len(ev): os.makedirs(out+'/Evaluation', exist_ok=True)
+print("Done Evaluating. Saving Plots.")
+for name, figure in tqdm(figures.items()): figure.write_image(f'{out}/{name}.pdf')
