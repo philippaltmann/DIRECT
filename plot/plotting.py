7fe10bc21d05e895200942de940935284df41bae
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-31 21:59:03 +0100

    Added termination pie plot, updated layouts

--- plot/plotting.py
+++ plot/plotting.py
@@ -1,14 +1,26 @@
 from safety_env.plotting import heatmap_3D
 import numpy as np; import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-title = lambda plot: [plot["metric"], plot["title"]]
+
+title = lambda plot, y=None: [y or plot["metric"], plot["title"]]
 
 # Helper functions to create scatters/graphs from experiment & metric
-def plot_box(plot):
+def plot_box(plot, y=None):
   box = lambda g: go.Box(name=g['label'], y=g['data'][0], marker_color=color(g['hue']), boxmean=True) 
-  figure = go.Figure(layout=layout(**dict(zip(['y','title'], title(plot)))), data=[box(g) for g in plot['graphs']])
+  figure = go.Figure(layout=layout( y=y or plot['metric']), data=[box(g) for g in plot['graphs']])
+  # **dict(zip(['y','title'], title(plot,y)))
   figure.add_hline(y=plot['graphs'][0]['data'][1], line_dash = 'dash', line_color = 'rgb(64, 64, 64)')
   return {' '.join(title(plot)): figure}
+
+
+def plot_pie(data):
+  labels = [str(l).split('.')[-1] for l in data[0]['data'].keys()]; titles = [ d['label'] for d in data]
+  colors = {'colors':[color({'GOAL': 140, 'TIME': 60, 'FAIL': 350}[l]) for l in labels]}
+  figure = make_subplots(rows=1, cols=len(data), subplot_titles=titles, specs=[[{'type':'domain'}]*len(data)])
+  [figure.add_trace(go.Pie(labels=labels, values=list(d['data'].values()), name=d['label'], hole=.4, marker=colors), 1, i+1) for i,d in enumerate(data)]
+  figure.update_layout(margin=dict(l=8, r=8, t=8, b=8), width=600, height=200) #annotations=titles, 
+  return figure   
 
 
 def smooth(data, degree=4):
@@ -28,13 +40,16 @@ def plot_ci(plot):
   return {' '.join(title(plot)): figure}
 
 
-# Helper functions to create scatters/graphs from experiment & metric
 def plot_eval(plot):
-  plot['title'] = plot['title'] + plot['metric']; plot['metric'] = 'Reward'; 
-  return plot_box(plot)
+  box = list(plot_box({**plot, 'graphs': [{**g, 'data': g['data'][:2]}for g in plot['graphs']]}, 'Mean Return').values())[0]
+  pie = plot_pie([{'label': g['label'], 'data': g['data'][2]}for g in plot['graphs']])
+  _t = lambda type: f"Evaluation/{type} {plot['metric']} ({plot['title']})"
+  return { _t('Termination'): pie,_t('Reward'): box }
+  # plot['title'] =''#= plot['title'] + plot['metric']; plot['metric'] = 'Reward'; 
+  return dict(zip([plot['metric']],plot_box(plot, 'Mean Return').values()))
 
 
-def plot_heatmap(plot): (data, (vmin,vmax)) = plot['data']; return {f'Heatmaps/{plot["title"]}': heatmap_3D(data, vmin, vmax)}
+def plot_heatmap(plot): return {f'Heatmaps/{plot["title"]}': heatmap_3D(plot['data'], compress=True)}
 
 
 #                      Red         Orange     Green          Blue        Purple 
@@ -46,6 +61,9 @@ def layout(title=None, legend=True, wide=True, x='', y=''):
   axis = lambda title: {'gridcolor': 'rgba(64, 64, 64, 0.32)', 'linecolor': 'rgb(64, 64, 64)', 'title': title,
     'mirror':True, 'ticks':'outside', 'showline':True, 'zeroline': True, 'zerolinecolor': 'rgba(64, 64, 64, 0.32)'} 
     #'tickmode': 'linear', 'range':[-0.5,max(data.shape)-0.5], 
+  return go.Layout( title=title, showlegend=legend, font=dict(size=24), 
+    margin=dict(l=8, r=8, t=8+(72 * (title is not None)), b=8), width=600+200*wide+100*legend, height=400, 
+    xaxis=axis(x), yaxis=axis(y), plot_bgcolor='rgba(64,64,64,0.04)') #, paper_bgcolor='rgba(0,0,0,0)', 
    
   return go.Layout( title=title, showlegend=legend, font=dict(size=24), 
     margin=dict(l=8, r=8, t=8+(72 * (title is not None)), b=8), width=900+300*wide+300*legend, height=600, 
