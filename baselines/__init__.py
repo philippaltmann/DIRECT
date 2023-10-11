aca3800af9f42b5ba4f349162075c5c3d96b5bb2
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-11 13:12:21 +0200

    Added SAC Baseline

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,8 +1,9 @@
 from .a2c import A2C
 from .dqn import DQN
 from .ppo import PPO
+from .sac import SAC
 from .sil import SIL
 from .vpg import VPG
 from .direct import DIRECT
 
-ALGS = ['DIRECT', 'A2C', 'DQN', 'PPO', 'SIL', 'VPG']
+ALGS = ['DIRECT', 'A2C', 'DQN', 'PPO', 'SAC', 'SIL', 'VPG']
