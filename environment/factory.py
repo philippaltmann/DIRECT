dff7a5df742140f954d4529876de0aa2ce9a9425
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-11 14:17:01 +0200

    Upodated env factory

--- environment/factory.py
+++ environment/factory.py
@@ -10,10 +10,10 @@ def make_vec(name, seed=None, n_envs=1, **kwargs):
   spec = lambda rank: {**named(name), 'seed': seed+rank, **kwargs}
   return DummyVecEnv([_make(**spec(i)) for i in range(n_envs)])
 
-def factory(env_spec, n_train=4, explore=False, **kwags):
+def factory(env_spec, n_train=4, **kwags):
   assert len(env_spec) > 0, 'Please specify at least one environment for training'
   test_names = ['validation', *[f'evaluation-{i}' for i in range(len(env_spec)-1)]]
-  return { 'train': make_vec(env_spec[0], n_envs=n_train, explore=explore, **kwags), 
+  return { 'train': make_vec(env_spec[0], n_envs=n_train, **kwags), 
     'test': {name: make_vec(spec, render_mode='2D', record_video=True, **kwags) for name, spec in zip(test_names, env_spec)}
   }   
 
