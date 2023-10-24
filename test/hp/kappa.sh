12397cd2c0ac9c06abbc5f2171803651f6416b5f
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-24 16:11:32 +0200

    Updated kappa eval script

--- test/hp/kappa.sh
+++ test/hp/kappa.sh
@@ -2,23 +2,14 @@
 # Envs: Maze9Sparse, PointMaze9 (Scale 4, reward, steps, hps, training steps)
 P="results/1-HP/kappa"; mkdir -p "$P"
 
-# Grid Maze
-for RUN in 1 2 3 4; do
-  for ENV in "Maze9Sparse"; do
-    O="results/1-HP/out/kappa/$ENV"; mkdir -p "$O"
-    for KAPPA in 256 512 1024 2048 4096; do
-      nohup python -m run DIRECT --kappa $KAPPA -e $ENV -t 24 -s $RUN --path $P &> "$O/$KAPPA-$RUN.out" &
-      sleep 5
-    done
-  done
-done
-
 # Point Maze
-for RUN in 1 2 3 4; do
-  for ENV in "PointMaze9"; do
+for RUN in 5 6 7 8; do #1 2 3 4
+  for E in "Maze9Sparse 1" "PointMaze9 4"; do
+    set -- $E; ENV=$1; F=$2;
     O="results/1-HP/out/kappa/$ENV"; mkdir -p "$O"
-    for KAPPA in 32 128 1024 8192 32768; do
-      nohup python -m run DIRECT --kappa $KAPPA -e $ENV -t 96 -s $RUN --path $P &> "$O/$KAPPA-$RUN.out" &
+    for KAPPA in 8 32 256 2048 8192; do
+      K=$((KAPPA*F)); T=$((F*24));
+      nohup python -m run DIRECT --kappa $K -e $ENV -t $T -s $RUN --path $P &> "$O/$K-$RUN.out" &
       sleep 5
     done
   done
