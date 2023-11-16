efbc8b2758201ed8ce33b643876643459eb8af3b
Philipp <philipp.altmann@ifi.lmu.de>
2023-11-16 17:39:27 +0100

    Updated baselines

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,9 +1,8 @@
+from .direct import DIRECT
+from .gasil import GASIL
+from .sil import SIL
+from .ppo import PPO
 from .a2c import A2C
 from .dqn import DQN
-from .ppo import PPO
-from .sac import SAC
-from .sil import SIL
-from .vpg import VPG
-from .direct import DIRECT
 
-ALGS = ['DIRECT', 'A2C', 'DQN', 'PPO', 'SAC', 'SIL', 'VPG']
+ALGS = ['DIRECT','GASIL', 'SIL', 'PPO', 'A2C', 'DQN']
