afe119d0a413673ecd925da3cca60630d5941796
Philipp <philipp.altmann@ifi.lmu.de>
2023-11-17 18:55:48 +0100

    Added Shift Benchmark Script

---
+++ test/2-bench/shift.sh
@@ -0,0 +1,9 @@
+# Benchmarking the exploration of shifting environments 
+P="results/2-bench"; mkdir -p "$P"; O="$P/out/HoleyGrid"; mkdir -p "$O"
+
+for RUN in 1 2 3 4 5 6 7 8; do
+  for ALG in 'DIRECT' 'GASIL' 'SIL' 'PPO' 'A2C'; do 
+    nohup python -m run $ALG -e HoleyGrid HoleyGridShift -t 8 -s $RUN --path $P &> "$O/$ALG-$RUN.out" &
+    sleep 2
+  done
+done
