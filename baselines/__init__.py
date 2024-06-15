86da30692a6e0883c0cc7fe827fd287670fb1c87
Philipp <philipp.altmann@ifi.lmu.de>
2024-06-15 14:13:05 +0200

    Added RLHF Baseline

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -4,5 +4,6 @@ from .sil import SIL
 from .ppo import PPO
 from .a2c import A2C
 from .dqn import DQN
+from .pref import PrefPPO
 
-ALGS = ['DIRECT','GASIL', 'SIL', 'PPO', 'A2C', 'DQN']
+ALGS = ['DIRECT','GASIL', 'SIL', 'PPO', 'A2C', 'DQN', 'PrefPPO']
